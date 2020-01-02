9e40f4713345a5b9aebbdc7435052f7690fb4cb8
Hayes Neuman <neurosie@gmail.com>
2020-01-01 22:57:53 -0500

    Add exercise text

--- src/cacher.rs
+++ src/cacher.rs
@@ -1,7 +1,22 @@
 use std::cmp::Eq;
-
 use std::collections::HashMap;
 use std::hash::Hash;
+
+/*
+Try modifying Cacher to hold a hash map rather than a single value. The keys of
+the hash map will be the arg values that are passed in, and the values of the
+hash map will be the result of calling the closure on that key. Instead of
+looking at whether self.value directly has a Some or a None value, the value
+function will look up the arg in the hash map and return the value if it’s
+present. If it’s not present, the Cacher will call the closure and save the
+resulting value in the hash map associated with its arg value.
+
+The second problem with the current Cacher implementation is that it only
+accepts closures that take one parameter of type u32 and return a u32. We might
+want to cache the results of closures that take a string slice and return usize
+values, for example. To fix this issue, try introducing more generic parameters
+to increase the flexibility of the Cacher functionality.
+*/
 
 struct Cacher<F, A, R>
 where
